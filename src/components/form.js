4a933a1546629ae5e85e984179f7c972bd81bd5b
Rodrigo Sonoki <rosonoki@gmail.com>
2020-07-12 14:15:36 -0300

    feat: react hook form

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { useForm } from "react-hook-form"
 import Title from "../components/title"
 
 const Container = styled.div`
@@ -86,6 +87,9 @@ const Container = styled.div`
 `
 
 function Form() {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => console.log(data)
+
   return (
     <div>
       <Container>
@@ -94,23 +98,41 @@ function Form() {
             h1="Quer criar sua loja?"
             p="Precisamos dos seus dados de contato pra que possamos fazer o setup da sua loja e incluí-la no nosso marketplace."
           />
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="name-input">
               Nome
-              <input id="name-input" type="text" placeholder="Rodrigo Sonoki" />
+              <input
+                name="name"
+                id="name-input"
+                type="text"
+                placeholder="Rodrigo Sonoki"
+                ref={register({ required: true })}
+              />
             </label>
+            {errors.exampleRequired && <span>This field is required</span>}
             <label htmlFor="email-input">
               E-mail
               <input
+                name="email"
                 id="email-input"
                 type="email"
                 placeholder="rodrigo@harver.com.br"
+                ref={register({ required: true })}
               />
             </label>
+            {errors.exampleRequired && <span>This field is required</span>}
             <label htmlFor="phone-input">
               WhatsApp
-              <input id="phone-input" type="tel" placeholder="(11)99725-0212" />
+              <input
+                name="phone"
+                id="phone-input"
+                type="tel"
+                placeholder="(11)99725-0212"
+                ref={register({ required: true })}
+              />
+              {errors.exampleRequired && <span>This field is required</span>}
             </label>
+            <input type="submit" />
             <button>ENVIAR</button>
           </form>
         </div>
