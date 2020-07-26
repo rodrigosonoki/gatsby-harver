8423fad93ed23d23020b30fbbc2a5ce6549afbc6
Rodrigo Sonoki <rodrigosonoki@iMac-de-Rodrigo.local>
2020-07-26 15:36:10 -0300

    fix: web-font-loader plugin install

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,14 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Montserrat", "Poppins"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,8 +1,7 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+
 
 * {
 box-sizing: border-box;
