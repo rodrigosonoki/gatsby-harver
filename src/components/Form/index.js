83b1babfe386f60ea78b5cbd2701d7c5278b6a37
Rodrigo Sonoki <rodrigosonoki@iMac-de-Rodrigo.local>
2020-07-29 19:04:06 -0300

    fix: env var

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,61 +1,119 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { useForm } from "react-hook-form"
 import Title from "../Title"
 
-import { Container } from "./styles"
+import { Container, Content, StyledCheck } from "./styles"
 
 function Form() {
+  /* HOOKS
+   */
+
+  //useState
   const { register, handleSubmit, errors } = useForm()
-  const onSubmit = data => console.log(data)
+  const [apiResponse, setApiResponse] = useState("")
+
+  //FUNCTIONS
+  const onSubmit = data => {
+    fetch(process.env.API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Rodrigo",
+        email: "rosonoki@gmail.com",
+        phone: "11997250212",
+      }),
+    })
+      .then(console.log(data))
+      .then(response => response.json())
+      .then(data => setApiResponse(data))
+      .then(console.log(process.env.API_URL))
+  }
 
   return (
-    <div>
-      <Container>
-        <div>
-          <Title
-            h1="Quer criar sua loja?"
-            p="Precisamos dos seus dados de contato pra que possamos fazer o setup da sua loja e incluí-la no nosso marketplace."
-          />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name-input">
-              Nome
-              <input
-                name="name"
-                id="name-input"
-                type="text"
-                placeholder="Rodrigo Sonoki"
-                ref={register({ required: true })}
+    <Container>
+      {(() => {
+        if (apiResponse.code === "200") {
+          return (
+            <Content>
+              <StyledCheck>
+                <svg
+                  className="checkmark"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 52 52"
+                >
+                  <circle
+                    className="checkmark__circle"
+                    cx="26"
+                    cy="26"
+                    r="25"
+                    fill="none"
+                  />
+                  <path
+                    className="checkmark__check"
+                    fill="none"
+                    d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                  />
+                </svg>
+              </StyledCheck>
+              <span>
+                Obrigado pelo interesse! <br />
+                Entraremos em contato o mais rápido possível com você!
+              </span>
+            </Content>
+          )
+        } else {
+          return (
+            <div>
+              <Title
+                h1="Quer criar sua loja?"
+                p="Precisamos dos seus dados de contato pra que possamos fazer o setup da sua loja e incluí-la no nosso marketplace."
               />
-            </label>
-            {errors.exampleRequired && <span>This field is required</span>}
-            <label htmlFor="email-input">
-              E-mail
-              <input
-                name="email"
-                id="email-input"
-                type="email"
-                placeholder="rodrigo@harver.com.br"
-                ref={register({ required: true })}
-              />
-            </label>
-            {errors.exampleRequired && <span>This field is required</span>}
-            <label htmlFor="phone-input">
-              WhatsApp
-              <input
-                name="phone"
-                id="phone-input"
-                type="tel"
-                placeholder="(11)99725-0212"
-                ref={register({ required: true })}
-              />
-              {errors.exampleRequired && <span>This field is required</span>}
-            </label>
-            <button>ENVIAR</button>
-          </form>
-        </div>
-      </Container>
-    </div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="name-input">
+                  Nome
+                  <input
+                    name="name"
+                    id="name-input"
+                    type="text"
+                    placeholder="Rodrigo Sonoki"
+                    ref={register({ required: true })}
+                  />
+                </label>
+                {errors.exampleRequired && <span>This field is required</span>}
+                <label htmlFor="email-input">
+                  E-mail
+                  <input
+                    name="email"
+                    id="email-input"
+                    type="email"
+                    placeholder="rodrigo@harver.com.br"
+                    ref={register({ required: true })}
+                  />
+                </label>
+                {errors.exampleRequired && <span>This field is required</span>}
+                <label htmlFor="phone-input">
+                  WhatsApp
+                  <input
+                    name="phone"
+                    id="phone-input"
+                    type="tel"
+                    placeholder="(11)99725-0212"
+                    ref={register({ required: true })}
+                  />
+                  {errors.exampleRequired && (
+                    <span>This field is required</span>
+                  )}
+                </label>
+                <button type="submit">ENVIAR</button>
+              </form>
+            </div>
+          )
+        }
+      })()}
+    </Container>
   )
 }
 
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  height: 100%;
+  height: 664px;
   background: rgb(255, 153, 102);
   background: linear-gradient(
     144deg,
@@ -85,5 +85,81 @@ export const Container = styled.div`
   @media only screen and (max-width: 690px) {
     form {
     }
+  }
+`
+
+export const StyledCheck = styled.div`
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .checkmark {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    display: block;
+    stroke-width: 2;
+    stroke: #f17174;
+    stroke-miterlimit: 10;
+    box-shadow: inset 0px 0px 0px #fff;
+    animation: fill 0.4s ease-in-out 0.4s forwards,
+      scale 0.3s ease-in-out 0.9s both;
+  }
+
+  .checkmark__circle {
+    stroke-dasharray: 166;
+    stroke-dashoffset: 166;
+    stroke-width: 2;
+    stroke-miterlimit: 10;
+    stroke: #fff;
+    fill: none;
+    animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+  }
+
+  .checkmark__check {
+    transform-origin: 50% 50%;
+    stroke-dasharray: 48;
+    stroke-dashoffset: 48;
+    animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+  }
+
+  @keyframes stroke {
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+
+  @keyframes scale {
+    0%,
+    100% {
+      transform: none;
+    }
+    50% {
+      transform: scale3d(1.1, 1.1, 1);
+    }
+  }
+
+  @keyframes fill {
+    100% {
+      box-shadow: inset 0px 0px 0px 30px #fff;
+    }
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 160px;
+  justify-content: space-between;
+  margin: auto;
+  span {
+    color: #fff;
+    font-family: Montserrat;
+    font-weight: 700;
+    font-size: 24px;
   }
 `
